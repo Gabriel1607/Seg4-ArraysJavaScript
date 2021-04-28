20ce47985672b897faade90be81483e01cb28e96
Gabriel <gabrielmora2001@hotmail.com>
2021-04-28 14:29:40 -0500

    Primer commit

---
+++ src/js/view/MyRect.js
@@ -0,0 +1,20 @@
+class MyRect {
+    constructor(posX,posY,tam){
+        this.posX = posX;
+        this.posY = posY;
+        this.tam  = tam;
+    }
+
+    draw(){
+        fill(255,0,0);
+        rect(this.posX,this.posY,this.tam,this.tam);
+    }
+
+    getTam(){
+        return this.tam;
+    }
+
+    setTam(tam){
+        this.tam = tam;
+    }
+}
